9be0f05ab64444ad8fb45702a1d0aaa48de39653
Cynthia Uche <61497064+thegirlSynth@users.noreply.github.com>
2022-07-13 04:45:11 +0200

    Update printLetters.c

--- printLetters.c
+++ printLetters.c
@@ -1,12 +1,40 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
 
-/**
- * print_number - prints a number send to this function
- * @args: List of arguments
- * Return: The number of arguments printed
- */
+/* Printing a Character */
 
-int print_number(va_list args)
+int printchar(va_list list)
+{
+	return (_putchar(va_arg(list, int)));
+}
+
+/* Printing a String */
+
+int printstr(va_list list)
+{
+	int i, n = 0;
+
+	char *s = va_arg(list, char *);
+
+	if (s == NULL)
+		s = "(null)";
+    
+  else if (*s == '\0')
+    return (-1);
+  
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		n += _putchar(s[i]);
+	}
+	return (n);
+}
+
+
+/* Printing a Number */
+
+int printnum(va_list args)
 {
 	int n;
 	int div;
@@ -32,3 +60,7 @@ int print_number(va_list args)
 		div /= 10;
 	}
         return (len);
+}
+
+
+
